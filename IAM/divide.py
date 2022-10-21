5e102fbf6e824bee3104aac4c6e0f5ccd62f2cfc
Daniel Rosa <dan.rosa10@gmail.com>
2022-10-20 23:47:19 -0300

    uup

---
+++ IAM/divide.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+with open("validationset2.txt", "r") as f:
+	trains = f.readlines()
+	f.close()
+
+images = []
+for item in trains:
+	item = item.split('-')[:-1]
+	item = "-".join(item)+".png"
+	images.append(item)
+
+for img in images:
+	try:
+		origin = "Dataset/"+img
+		destin = "val/"+img
+		shutil.move(origin, destin)
+		print(img)
+	except:
+		a = 1
