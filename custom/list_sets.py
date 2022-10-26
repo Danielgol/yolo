17c72a102410b9104e4fadef494e4dbec264cdae
Daniel Rosa <dan.rosa10@gmail.com>
2022-10-26 14:53:20 -0300

    divide

--- custom/list_sets.py
+++ custom/list_sets.py
@@ -10,3 +10,15 @@ for img in imgs:
         with open("train.txt", "a+") as f:
                 f.write(abso+'\n')
                 f.close()
+
+
+imgs = os.listdir("images/val")
+
+for img in imgs:
+        abso = os.path.abspath(img)
+        abso = abso.split("/")[:-1]
+        abso =  "/".join(abso)+"/images/val/"+img
+        #print(abso)
+        with open("val.txt", "a+") as f:
+                f.write(abso+'\n')
+                f.close()
