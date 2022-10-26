c3d5992c18c840e477da0a6e8f25a776aac37436
Daniel Rosa <dan.rosa10@gmail.com>
2022-10-26 14:50:30 -0300

    divide

--- custom/divide.py
+++ custom/divide.py
@@ -20,3 +20,24 @@ for img in images:
 		
 	except:
 		a = 1
+
+
+with open("validationset1.txt", "r") as f:
+	trains = f.readlines()
+	f.close()
+
+images = []
+for item in trains:
+	item = item.split('-')[:-1]
+	item = "-".join(item)+".png"
+	images.append(item)
+
+for img in images:
+	try:
+		origin = "IAM/"+img
+		destin = "images/val/"+img
+		print(origin, destin)
+		shutil.move(origin, destin)
+		
+	except:
+		a = 1
