e4d9009b7eee421dd96609cafc82f1efc945ce31
Daniel Rosa <danielgol@yahoo.com.br>
2023-05-12 19:59:59 -0300

    Create divide.py

---
+++ custom/divide.py
@@ -0,0 +1,51 @@
+import os
+import shutil
+import argparse
+
+# python divide.py --extension png
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--extension', type=str, default='png', help='Dataset Path')
+	opt = parser.parse_args()
+
+	ext = "."+opt.extension
+
+	open('train.txt', 'w').close()
+	open('test.txt', 'w').close()
+	open('val.txt', 'w').close()
+
+
+	for img in os.listdir("./labels/train"):
+		if "gitkeep" in img:
+			continue
+		img = img.split(".")[0]+ext
+		abso = os.path.abspath(img)
+		abso = abso.split("/")[:-1]
+		abso =  "/".join(abso)+"/images/train/"+img
+		with open("train.txt", "a+") as f:
+			f.write(abso+'\n')
+			f.close()
+
+	for img in os.listdir("./labels/val"):
+		if "gitkeep" in img:
+			continue
+		img = img.split(".")[0]+ext
+		abso = os.path.abspath(img)
+		abso = abso.split("/")[:-1]
+		abso =  "/".join(abso)+"/images/val/"+img
+		with open("val.txt", "a+") as f:
+			f.write(abso+'\n')
+			f.close()
+			
+	for img in os.listdir("./labels/test"):
+		if "gitkeep" in img:
+			continue
+		img = img.split(".")[0]+ext
+		abso = os.path.abspath(img)
+		abso = abso.split("/")[:-1]
+		abso =  "/".join(abso)+"/images/test/"+img
+		with open("test.txt", "a+") as f:
+			f.write(abso+'\n')
+			f.close()
